8ec85378c3fd93e18b39003f0460735521dddb1d
nongi3 <tangolamark@yandex.ru>
2021-09-05 13:33:23 +0700

    начали писать 21

--- hello.py
+++ hello.py
@@ -1,6 +1,39 @@
-def hello_world_f():
-    for i in range(15):
-        if i % 5 == 0:
-            print("Hello world")
+import random
 
-# asfafфыафыаasfgasasgagag
+
+def create_deck():
+    deck = []
+    for i in range(2, 11):
+        for j in ('C', 'P', 'B', 'X'):
+            deck.append(str(i)+j)
+    for i in ('J','Q','K', 'T'):
+        for j in ('C', 'P', 'B', 'X'):
+            deck.append(i+j)
+    return deck
+
+
+def started_hand(deck):
+    hand = []
+    for i in range(2):
+        hand.append(random.choice(deck))
+        deck.remove(hand[-1])
+    return hand, deck
+    
+
+
+def start_game():
+    deck = create_deck()
+    hand = []
+    hand, deck = started_hand(deck)
+    
+
+start_game()
+
+
+
+
+
+
+
+
+
